d676b1a648738819e70478f2145dc19382b12265
Nardos Birhanu <nardos.tunkala@gmail.com>
2025-02-25 14:19:41 +0100

    Update Cancel.py

--- Cancel.py
+++ Cancel.py
@@ -25,3 +25,49 @@ def cancel(pathn,paths):
 
     AudioSegment.converter = which("ffmpeg")
     
+
+    def duration_detector(file):
+        totalsec=0
+        with audioread.audio_open(file) as f:
+
+            # totalsec contains the length in float
+            totalsec = f.duration
+        length=int(totalsec)
+        hours = length // 3600  # calculate in hours
+        length %= 3600
+        mins = length // 60  # calculate in minutes
+        length %= 60
+        seconds = length  # calculate in seconds
+
+        return hours, mins, seconds
+
+  
+    x,y,z=duration_detector(paths)
+    a,b,c=duration_detector(pathn)
+    
+    
+    while x>a or y>b or z>c:
+        
+        noi+=noi
+        if x<a or (x==a and y<b) or (x==a and y==b and z<c):
+            break
+        if c>=60:
+            b+=1
+        elif b>=0:
+            a+=1
+        else:
+            a+=a
+            b+=b
+            c+=c
+       
+    noi+=noi
+
+    
+    invertednoise = noi.invert_phase()
+    combined = sig.overlay(invertednoise,position=0)
+    name="output.wav"
+    combined.export(name, format="wav")
+    return name
+
+
+    
